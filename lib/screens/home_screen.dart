400e55ad195f6a2ce7dff00d5e93ba53728b5b8d
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-07 20:08:14 +0300

    add logo image in the row

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,32 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Text('hello and good morning'),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/img_1.png'))),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
