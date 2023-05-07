aac3e7c89384ef58c80b311be7a1b1aac9889acd
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-05-07 13:21:22 +0300

    ad method to get the current index

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -7,20 +7,42 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 3;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+
+  void _ontappedItem(int index) {
+    _selectedIndex = index;
+    print('The tapped index is : $_selectedIndex');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My tickets'),
       ),
-      body: Container(),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.airplane_ticket), label: 'ticket'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
-      ]),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _ontappedItem,
+          elevation: 10,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.blueGrey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.airplane_ticket), label: 'ticket'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+          ]),
     );
   }
 }
